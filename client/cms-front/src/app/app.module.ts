345ec8fe398b7bcba581350001a1970057591243
Austin <austinkryy@gmail.com>
2023-08-04 19:52:52 -0400

    added inital components

--- client/cms-front/src/app/app.module.ts
+++ client/cms-front/src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+import { ContactEditComponent } from './contact/contact-edit/contact-edit.component';
+import { ContactCreateComponent } from './contact/contact-create/contact-create.component';
+import { LoaderComponent } from './shared/loader/loader.component';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    ModalComponent,
+    ContactListComponent,
+    ContactDetailComponent,
+    ContactEditComponent,
+    ContactCreateComponent,
+    LoaderComponent,
+    AlertComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule
