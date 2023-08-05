e474e5b46982c18b5e98552435a40e78df76829c
Austin <austinkryy@gmail.com>
2023-08-05 15:57:20 -0400

    setup app component and inital page/routes

--- client/cms-front/src/app/app.module.ts
+++ client/cms-front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
 import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
@@ -13,13 +12,20 @@ import { ContactCreateComponent } from './contact/contact-create/contact-create.
 import { LoaderComponent } from './shared/loader/loader.component';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PaginationComponent } from './shared/pagination/pagination.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Define routes here
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' }, // redirect to '/contact' by default
+  { path: 'contacts', component: ContactListComponent }, // route for the Contact List
+  { path: 'contact/:id', component: ContactDetailComponent } // route for the Contact Detail w/ a param for the contact ID
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent,
     ModalComponent,
     ContactListComponent,
     ContactDetailComponent,
@@ -30,7 +36,8 @@ import { PaginationComponent } from './shared/pagination/pagination.component';
     PaginationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) // Add RouterModule with routes here
   ],
   providers: [],
   bootstrap: [AppComponent]
